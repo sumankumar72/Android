521d0dd079be86db7ac3005ecc3e2a4e3873fecc
Suman Shandilya <sumankumar72@gmail.com>
2017-09-11 01:24:22 +0530

    Fee Activity

--- app/src/main/java/suman/dev/strocks/FeeActivity.java
+++ app/src/main/java/suman/dev/strocks/FeeActivity.java
@@ -1,13 +1,22 @@
 package suman.dev.strocks;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -24,11 +33,16 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import suman.dev.strocks.Adapters.AttandanceAdapter;
 import suman.dev.strocks.Adapters.FeeAdapter;
+import suman.dev.strocks.Adapters.ResultPostAdapter;
 import suman.dev.strocks.Constant.Const;
+import suman.dev.strocks.Model.ChildModel;
 import suman.dev.strocks.Model.FeeDetail;
 import suman.dev.strocks.Model.ItemClickListener;
+import suman.dev.strocks.Model.UserClassData;
 import suman.dev.strocks.Model.UserProfile;
+import suman.dev.strocks.Model.UserSubjectData;
 import suman.dev.strocks.WebService.VolleyJsonObjectCallback;
 import suman.dev.strocks.WebService.VolleyService;
 
@@ -55,6 +69,12 @@ public class FeeActivity  extends AppCompatActivity implements View.OnClickListe
     private String SelectedMonth,CurrentMonth;
     private float AmountPaid=0;
 
+    private String classId;
+    private RecyclerView studentRecyclerView;
+    private RecyclerView.LayoutManager studentLayoutManager;
+    private AttandanceAdapter studentAdapter;
+    private ArrayList<ChildModel> students;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -63,6 +83,17 @@ public class FeeActivity  extends AppCompatActivity implements View.OnClickListe
         UserProfileHeader  profile =  new UserProfileHeader(this,findViewById(R.id.fee_userProfileHeader));
         profile.loadProfile();
 
+        if(UserProfile.Role.toLowerCase().equals("teacher")){
+            intiLayoutForTeacher();
+        }
+        else {
+            initLayoutForStudent();
+        }
+
+    }
+
+    //this function called when user role is student or parent
+    private void initLayoutForStudent(){
         SimpleDateFormat month_date = new SimpleDateFormat("MM");
         CurrentMonth = month_date.format(new Date());
 
@@ -94,10 +125,127 @@ public class FeeActivity  extends AppCompatActivity implements View.OnClickListe
         if(UserProfile.Role.toLowerCase().equals("student"))
             fee_btnPayNow.setEnabled(false);
 
-        //loadFee();
-
         fee_TotalAmount = (TextView)findViewById(R.id.fee_TotalAmount);
     }
+
+    private void intiLayoutForTeacher(){
+        RelativeLayout layoutStudentFee = (RelativeLayout)findViewById(R.id.layoutStudentFee);
+        layoutStudentFee.setVisibility(View.GONE);
+        LinearLayout layoutForTeacher = (LinearLayout)findViewById(R.id.layoutForTeacher);
+        layoutForTeacher.setVisibility(View.VISIBLE);
+
+        Spinner ddlClass = (Spinner) findViewById(R.id.ddlClass);
+        ArrayAdapter<UserClassData> spinerAdapter = new ArrayAdapter<UserClassData>(this, R.layout.support_simple_spinner_dropdown_item, UserProfile.Classes);
+        ddlClass.setAdapter(spinerAdapter);
+
+        try {
+            ddlClass.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                    classId= UserProfile.Classes.get(position).Id + "";
+                    loadStudents();
+                }
+
+                @Override
+                public void onNothingSelected(AdapterView<?> parent) {
+
+                }
+            });
+        } catch (Exception e) {
+
+        }
+    }
+    //This function execute when user loggedin as Teacher Role
+    private void loadStudents(){
+        students= new ArrayList<>();
+        service.MakeGetRequest(String.format(Const.GET_STUDENT_BY_CLASS, UserProfile.SessionData.Id, classId),
+                FeeActivity.this, new VolleyJsonObjectCallback() {
+                    @Override
+                    public void onSuccess(JSONObject result) {
+                        try{
+                            ChildModel child ;
+                            JSONArray array = result.getJSONArray("data");
+                            for(int i=0;i<array.length();i++){
+                                JSONObject studentJson =array.getJSONObject(i).getJSONObject("student");
+                                child =  new ChildModel(
+                                        studentJson.getString("user_token"),
+                                        studentJson.getString("first_name"),
+                                        studentJson.getString("middle_name"),
+                                        studentJson.getString("last_name")
+                                );
+                                students.add(child);
+                            }
+                            setAdapter(students);
+                        }
+                        catch (Exception e){
+                            e.getStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onError(VolleyError error) {
+                        String s = "";
+                    }
+                });
+    }
+    private void setAdapter(final ArrayList<ChildModel> students)
+    {
+        studentRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        studentRecyclerView.setHasFixedSize(true);
+        studentLayoutManager = new LinearLayoutManager(this);
+        studentRecyclerView.setLayoutManager(studentLayoutManager);
+
+        studentAdapter = new AttandanceAdapter(students, true);
+        studentRecyclerView.setAdapter(studentAdapter);
+
+        studentAdapter.setClickListener(new ItemClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+
+                createFeeDialog(students.get(position).UserToken);
+            }
+        });
+    }
+
+    private void createFeeDialog(final String StudentId){
+        try {
+            LinearLayout layout = new LinearLayout(FeeActivity.this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            final EditText txtReason = new EditText(FeeActivity.this);
+            txtReason.setHint("Reason");
+            layout.addView(txtReason);
+
+            final EditText txtAmount = new EditText(FeeActivity.this);
+            txtAmount.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+            txtAmount.setHint("Amount");
+            layout.addView(txtAmount);
+
+
+            AlertDialog.Builder dialog = new AlertDialog.Builder(FeeActivity.this);
+            dialog.setView(layout);
+            dialog.setTitle("Create Fine");
+            dialog.show();
+
+            dialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    Toast.makeText(FeeActivity.this, "", Toast.LENGTH_SHORT).show();
+                }
+            });
+
+
+//            final Dialog dialog = new Dialog(FeeActivity.this);
+//            dialog.setView(layout);;
+//            dialog.setTitle("Create Fine");
+//            dialog.show();
+        } catch (Exception e) {
+            e.getStackTrace();
+            FirebaseCrash.report(e);
+        }
+
+    }
+
 
     @Override
     public void onClick(View v) {
